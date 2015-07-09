45cad2714dbea6e3400c1af2853836edef40cb51
Brittany Tuohy <botuohy@gmail.com>
2015-07-09 18:08:20 -0500

    onRendered function
    Fixed bug where the visualization wasn’t showing. It was because the
    initialization code needed to be in the inRendered function of the
    template instead of the helpers.

--- lifetimeline.js
+++ lifetimeline.js
@@ -25,33 +25,26 @@ if (Meteor.isClient) {
     }
   });
 
-  Template.lifetimeline.helpers({
+  Template.lifetimeline.onRendered(function () {
     // DOM element where the Timeline will be attached
-    container: function () {
-      return document.getElementById('visualization');
-    },
+    var container = document.getElementById('visualization');
+    console.log("container: " + document.getElementById('visualization'));
 
     // Create a DataSet (allows two way data-binding)
-    items: function () {
-      return new vis.DataSet([
-        {id: 1, content: 'item 1', start: '2014-04-20'},
-        {id: 2, content: 'item 2', start: '2014-04-14'},
-        {id: 3, content: 'item 3', start: '2014-04-18'},
-        {id: 4, content: 'item 4', start: '2014-04-16', end: '2014-04-19'},
-        {id: 5, content: 'item 5', start: '2014-04-25'},
-        {id: 6, content: 'item 6', start: '2014-04-27', type: 'point'}
-      ]);
-    },
+    var items = new vis.DataSet([
+      {id: 1, content: 'item 1', start: '2014-04-20'},
+      {id: 2, content: 'item 2', start: '2014-04-14'},
+      {id: 3, content: 'item 3', start: '2014-04-18'},
+      {id: 4, content: 'item 4', start: '2014-04-16', end: '2014-04-19'},
+      {id: 5, content: 'item 5', start: '2014-04-25'},
+      {id: 6, content: 'item 6', start: '2014-04-27', type: 'point'}
+    ]);
 
     // Configuration for the Timeline
-    options: function () {
-      return {};
-    },
+    var options = {};
 
     // Create a Timeline
-    timeline: function () {
-      return new vis.Timeline(container, items, options);
-    }   
+    var timeline = new vis.Timeline(container, items, options);
   });
 }
 
