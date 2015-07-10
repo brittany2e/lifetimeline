6bdc031f783b51a85359c853907b552c21691c44
Brittany Tuohy <botuohy@gmail.com>
2015-07-10 10:23:59 -0500

    Further restructuring and adding config options
    Separated into functions

--- lifetimeline.js
+++ lifetimeline.js
@@ -28,20 +28,19 @@ if (Meteor.isClient) {
   Template.lifetimeline.onRendered(function () {
     // DOM element where the Timeline will be attached
     var container = document.getElementById('visualization');
-    console.log("container: " + document.getElementById('visualization'));
-
+    
     // Create a DataSet (allows two way data-binding)
-    var items = new vis.DataSet([
-      {id: 1, content: 'item 1', start: '2014-04-20'},
-      {id: 2, content: 'item 2', start: '2014-04-14'},
-      {id: 3, content: 'item 3', start: '2014-04-18'},
-      {id: 4, content: 'item 4', start: '2014-04-16', end: '2014-04-19'},
-      {id: 5, content: 'item 5', start: '2014-04-25'},
-      {id: 6, content: 'item 6', start: '2014-04-27', type: 'point'}
-    ]);
+    var items = initData();
 
     // Configuration for the Timeline
-    var options = {};
+    var options = {
+      height: '300px',
+      min: new Date(1880, 0, 1),             // lower limit of visible range
+      max: new Date(2015, 0, 1),             // upper limit of visible range
+      zoomMin: 1000 * 60 * 60 * 24 * 31 * 3, // about three months in milliseconds
+      zoomMax: 1000 * 60 * 60 * 24 * 31 * 12 * 100  // about 100 years in milliseconds
+    };
+
 
     // Create a Timeline
     var timeline = new vis.Timeline(container, items, options);
@@ -53,3 +52,17 @@ if (Meteor.isServer) {
     // code to run on server at startup
   });
 }
+
+initData = function() {
+  var items = new vis.DataSet([
+      {id: 1, content: 'John B. Oswald', start: 'year=1864', end: 'year=1932'},
+      {id: 2, content: 'item 2', start: '2014-04-14', end: '2014-04-19'},
+      {id: 3, content: 'item 3', start: '2014-04-18', end: '2014-04-19'},
+      {id: 4, content: 'item 4', start: '2014-04-16', end: '2014-04-19'},
+      {id: 5, content: 'item 5', start: '2014-04-25', end: '2014-04-19'},
+      {id: 6, content: 'item 6', start: '2014-04-27', type: 'point'}
+    ]);
+  return items;
+};
+
+
